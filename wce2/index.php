c934eeae20df7e168faaa99fcdcb34e56cc78174
x-man3757 <53595496+x-man3757@users.noreply.github.com>
2020-06-19 17:38:13 +0530

    Add files via upload

---
+++ wce2/index.php
@@ -0,0 +1,39 @@
+<?php include('allhead.php'); ?>
+
+<!-- Header Carousel -->
+<header id="myCarousel" class="carousel slide">
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner">
+		<div class="item active">
+			<div class="fill" style="background-image:url('images/1900x10801.png');"></div>
+			<div class="carousel-caption">
+				<a href="registrationform"><h2 style="color: white;">Register Today</h2></a>
+			</div>
+		</div>
+			
+</header>
+
+<!-- Page Content -->
+<div class="container" style="max-width: 1200px;">
+
+	
+
+	
+	<!-- Features Section -->
+	<div class="row">
+		<div class="col-lg-12">
+			<h2 class="page-header">STUDENT WELFARE SITE</h2>
+		</div>
+		<div class="col-md-6">
+			<p>STUDENT WELFARE SITE includes:</p>
+		</div>
+	</div>
+	<ol>
+		<li><button style="background-color:skyblue">hosstel form</button></li><br>
+		<li><button style="background-color:skyblue">college admission form</button></li><br>
+		<li><button style="background-color:skyblue">scholarship form</button></li>
+	</ol>
+	<hr>
+
+
+	<?php include('allfoot.php'); ?>
